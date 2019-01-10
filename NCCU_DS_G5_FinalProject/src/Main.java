034c7c91962e35a3a70e28d49d48a9fc6034ab1c
Jeremy <Jeremy@10.232.201.214>
2019-01-10 17:13:32 +0800

    1/10

---
+++ NCCU_DS_G5_FinalProject/src/Main.java
@@ -0,0 +1,10 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String GitHub = "https://github.com/chuangjeremy/NCCU_DS_G5_FinalProject.git";
+	}
+
+}
