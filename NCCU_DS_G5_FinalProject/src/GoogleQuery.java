eb9aaa0cbfb6da5fec32d72edf58224618d6c411
serragnarok <31920950+serragnarok@users.noreply.github.com>
2019-01-16 20:24:11 +0800

    final part

--- NCCU_DS_G5_FinalProject/src/GoogleQuery.java
+++ NCCU_DS_G5_FinalProject/src/GoogleQuery.java
@@ -17,10 +17,17 @@ public class GoogleQuery
 	public String searchKeyword;
 	public String url;
 	public String content;
+	public KeywordList keywordlist;
+	public ArrayList<Keyword> keywords = new ArrayList<Keyword>();
+	
+	
 	public GoogleQuery(String searchKeyword)
 	{
+		
 		this.searchKeyword = searchKeyword;
-		this.url = "http://www.google.com/search?q="+searchKeyword+ "+recipe" +"&oe=utf8&num=10";
+		this.url = "http://www.google.com/search?q="+searchKeyword + "+recipe" +"&oe=utf8&num=10";
+		this.keywordlist = new KeywordList(searchKeyword);
+		this.keywords = keywordlist.keywords;
 	}
 	
 	private String fetchContent() throws IOException
@@ -42,13 +49,13 @@ public class GoogleQuery
 		return retVal;
 	}
 	
-	public HashMap<String, String> query() throws IOException
+	public HashMap<String, Web> query() throws IOException
 	{
 		if(content==null)
 		{
 			content= fetchContent();
 		}
-		HashMap<String, String> retVal = new HashMap<String, String>();
+		HashMap<String, Web> retVal = new HashMap<String, Web>();
 		
 		Document doc = Jsoup.parse(content);
 		
@@ -62,9 +69,12 @@ public class GoogleQuery
 				String title = h3.text();
 				
 				Element cite = li.select("cite").get(0);
-				String citeUrl = cite.text();
+				String citeUrl = cite.text(); 
 //				System.out.println(title+" "+citeUrl);
-				retVal.put(title, citeUrl);
+				
+				int score = getsublinkScore(citeUrl);
+				Web data = new Web(citeUrl, title, score);
+				retVal.put(title, data);
 				
 			} catch (IndexOutOfBoundsException e) {
 				
@@ -73,22 +83,65 @@ public class GoogleQuery
 		}
 		
 		return retVal;
+		
 	}
 	
-	public ArrayList<String> getSubLink(URL url) throws IOException{
+	private int getsublinkScore(String citeUrl) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public double getLinkScore(String url) throws IOException{
+		
+		 
+		  WebPage home = new WebPage(url);
+		  home.setScore(this.keywords);
+		
+		
 		  ArrayList<String> subLinkArray= new ArrayList<String>() ;
-		  if(content==null) {
-			  content=fetchContent();
-		  }
+		  WordCounter temp_1 = new WordCounter(url);
+		  String sublinkContent_1;
+		  sublinkContent_1 = temp_1.fetchContent();
+		  
 		  int indexOfOpen=0;
-		  while((indexOfOpen=content.indexOf("a href=\"",indexOfOpen))!=-1) {
+		  while((indexOfOpen=sublinkContent_1.indexOf("a href=\"",indexOfOpen))!=-1) {
 			  indexOfOpen=indexOfOpen+8;
-			  int indexOfClose=content.indexOf("\"", indexOfOpen);
-			  String subLink=content.substring(indexOfOpen, indexOfClose);
+			  int indexOfClose=sublinkContent_1.indexOf("\"", indexOfOpen);
+			  String subLink=sublinkContent_1.substring(indexOfOpen, indexOfClose);
 			  subLinkArray.add(subLink);
 			  indexOfOpen=indexOfClose;
 		  }
-		  return subLinkArray;
+		  
+		  int firstSize = subLinkArray.size();
+		  
+		  for(int i=0;i<firstSize;i++) {
+			  
+			  ArrayList<String> subLinkArray_2= new ArrayList<String>() ;
+			  WordCounter temp_2 = new WordCounter(subLinkArray.get(i));
+			  String sublinkContent_2;
+			  sublinkContent_2 = temp_2.fetchContent();
+			  int indexOfOpen_2=0;
+			  while((indexOfOpen_2=sublinkContent_2.indexOf("a href=\"",indexOfOpen_2))!=-1) {
+				  
+				  indexOfOpen_2=indexOfOpen_2+8;
+				  int indexOfClose=sublinkContent_2.indexOf("\"", indexOfOpen_2);
+				  String subLink=sublinkContent_2.substring(indexOfOpen_2, indexOfClose);
+				  subLinkArray.add(subLink);
+				  indexOfOpen=indexOfClose;
+			  }
+		  }
+		  
+		  int lastSize = subLinkArray.size();
+		  double sublinkScore = 0;
+		  for(int i=0;i<lastSize;i++) {
+			 WebPage link = new WebPage(subLinkArray.get(i));
+			 link.setScore(this.keywords);
+			 sublinkScore += link.score;
+		  }
+		  
+		  sublinkScore += home.score;
+		  
+		  return sublinkScore;
 	  }
 	
 	
