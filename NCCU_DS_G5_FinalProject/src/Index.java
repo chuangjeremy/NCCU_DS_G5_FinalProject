ee1e4580cd920d31e8547692e4b17a991148dc9b
serragnarok <31920950+serragnarok@users.noreply.github.com>
2019-01-17 05:36:40 +0800

    final

--- NCCU_DS_G5_FinalProject/src/Index.java
+++ NCCU_DS_G5_FinalProject/src/Index.java
@@ -14,12 +14,12 @@ import javax.servlet.http.HttpServletResponse;
  * Servlet implementation class TestProject
  */
 @WebServlet("/TestProject")
-public class TestProject extends HttpServlet {
+public class Index extends HttpServlet {
 	private static final long serialVersionUID = 1L;
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public TestProject() {
+    public Index() {
         super();
         // TODO Auto-generated constructor stub
     }
@@ -47,10 +47,22 @@ public class TestProject extends HttpServlet {
 ////			keyword = request.getParameter("food")+ request.getParameter("cook")+request.getParameter("key");}
 //		}
 //			
+		
+//	try {
+//		GoogleQuery hi = new GoogleQuery("pork");
+//		double hello = hi.getLinkScore("http://www.recipe.com/");
+//		System.out.print(hello);
+//	}catch(Exception e) {
+//		
+//	}
+	
+	System.out.print("down");
+	
 	
 		String keyword = request.getParameter("keyword1").replace(" ", "")  + request.getParameter("cook").replace(" ", "") + request.getParameter("keyword2").replace(" ", "")+ request.getParameter("food").replace(" ", "");
 		GoogleQuery google = new GoogleQuery(keyword);
 		HashMap<String, Web> query = google.query();
+		
 		
 		
 		KeywordHeap sort = new KeywordHeap();
